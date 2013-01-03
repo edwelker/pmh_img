6db1356ed1ee8f6f65933d7fe1566c3038bb6693
Eddie Welker <eddie.welker@gmail.com>
2013-01-02 19:38:50 -0500

    added templates

--- manage.py
+++ manage.py
@@ -2,6 +2,12 @@
 import os
 import sys
 
+sys.path.insert(0, '/home/welkere/python')
+sys.path.insert(0, '/home/welkere/python/pmh_img')
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pmh_img.settings")
+
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pmh_img.settings")
 
--- pmh_img.py
+++ pmh_img.py
@@ -8,17 +8,17 @@ homedir = '/home/welkere'
 #the name of the current directory is the same as the env name
 env_name =  os.path.basename(os.path.normpath(os.path.dirname(__file__)))
 
-venv_loc = homedir + '/env/' + env_name
-activate_loc = venv_loc + '/bin/activate_this.py'
-info_parent_loc = homedir + '/python'
-info_loc = info_parent_loc + '/' + env_name
-settings = env_name + '.settings'
+#venv_loc = homedir + '/env/' + env_name
+#activate_loc = venv_loc + '/bin/activate_this.py'
+#info_parent_loc = homedir + '/python'
+#info_loc = info_parent_loc + '/' + env_name
+#settings = env_name + '.settings'
 
-#print venv_loc
-#print activate_loc
-#print info_parent_loc
-#print info_loc
-#print settings
+venv_loc = '/home/welkere/env/pmh_img'
+activate_loc = '/home/welkere/env/pmh_img/bin/activate_this.py'
+parent_loc = '/home/welkere/python'
+pmh_img_loc = '/home/welkere/python/pmh_img'
+settings = 'pmh_img.settings'
 
 os.environ['VIRTUAL_ENV'] = venv_loc 
 
@@ -31,14 +31,14 @@ _project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Add a custom Python path.
 sys.path.insert(0, _project_dir)
-sys.path.insert(0, info_loc)
+sys.path.insert(0, pmh_img_loc)
 #need to do this explicitly, since _project_dir is gonna be my staff directory,
 #because that's where the cgi is 'running'
-sys.path.insert(0, info_parent_loc)
+sys.path.insert(0, parent_loc)
 sys.path.insert(0, os.path.dirname(_project_dir))
 
 # Switch to the directory of your project. (Optional.)
-os.chdir(info_loc)
+os.chdir(pmh_img_loc)
 
 # Set the DJANGO_SETTINGS_MODULE environment variable.
 os.environ['DJANGO_SETTINGS_MODULE'] = settings
