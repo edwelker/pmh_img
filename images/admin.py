66b990c3202fe7f77762c6edefbf34c7b680215d
Eddie Welker <eddie.welker@gmail.com>
2013-01-03 18:28:16 -0500

    demo done

--- images/admin.py
+++ images/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from images.models import Image
 
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ('new_filename', 'image', 'img_thumb')
-    search_fields = ('new_filename', 'image')
+    list_display = ('new_filename', 'img_thumb', 'terms', 'orig_filename', 'med_url', 'included')
+    list_display_links = ('img_thumb',)
+    list_editable = ('included', 'terms')
+    search_fields = ('new_filename', 'orig_filename', 'terms')
+    list_filter = ('included',)
+
 
 admin.site.register(Image, ImageAdmin)
+admin.site.disable_action('delete_selected')
--- images/models.py
+++ images/models.py
@@ -1,16 +1,50 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill, ResizeToFit
+
+#helper method
+def generate_link(image):
+    return u"<a href='%s' target='_blank'>%s</a>" % (image.url, image.url.split('/')[-1])
+    
 
 # Create your models here.
 class Image(models.Model):
     image = models.ImageField(upload_to='originals')
-    new_filename = models.CharField(max_length=100, help_text="The new filename")
+
+    included = models.BooleanField(help_text="Check if the image should be included in PMH")
+    new_filename = models.CharField(max_length=100, blank=True, null=True, help_text="The desired new filename [don't include the '.jpg']")
+
+    terms = models.CharField(max_length=200, blank=True, help_text="A comma separated list of terms this image belongs to.")
+    
+    #The alternate sizes of the images
+    medium_thumb = ImageSpecField([ResizeToFit(width=300, height=400)], image_field='image', cache_to='medium_thumbs')
+    thumbnail = ImageSpecField([ResizeToFill(130, 130)], image_field='image', cache_to='thumbnails')
 
     def img_thumb(self):
-        #will need to be changed to the thumbnail once generated
         if self.image:
-            return u'<img src="/media/%s" />' % self.image
+            #this is hacky, need to reverse lookup the image
+            return u'<a href="%s/"><img src="%s" /></a>' % (self.id, self.thumbnail.url)
         else:
             return u'(No image)'
 
-    img_thumb.short_description = 'Thumb'
+    img_thumb.short_description = 'Thumbnail'
     img_thumb.allow_tags = True
+
+    def orig_filename(self):
+        return generate_link(self.image)
+
+    orig_filename.short_description = "Original"
+    orig_filename.allow_tags = True
+
+    def med_url(self):
+        return generate_link(self.medium_thumb) 
+    
+    med_url.short_description = "Medium"
+    med_url.allow_tags = True
+
+    def __unicode__(self):
+        return u"%s" % self.image.url.split('/')[-1]
+
+    class Meta:
+        ordering = ['-included']
