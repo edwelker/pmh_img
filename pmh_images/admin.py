c3ab56931de52bea4b4fdca28dcfeaec9c4c2d97
Eddie Welker <eddie.welker@gmail.com>
2013-01-03 00:47:25 -0500

    renamed model field

--- pmh_images/admin.py
+++ pmh_images/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from pmh_images.models import Image
 
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ('new_filename', 'original_filename', 'img_thumb')
-    search_fields = ('new_filename', 'original_filename')
+    list_display = ('new_filename', 'image', 'img_thumb')
+    search_fields = ('new_filename', 'image')
 
 admin.site.register(Image, ImageAdmin)
--- pmh_images/models.py
+++ pmh_images/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-    original_filename = models.ImageField(upload_to='originals')
+    image = models.ImageField(upload_to='originals')
     new_filename = models.CharField(max_length=100, help_text="The new filename")
 
     def img_thumb(self):
         #will need to be changed to the thumbnail once generated
-        if self.original_filename:
-            return u'<img src="/media/%s" />' % self.original_filename
+        if self.image:
+            return u'<img src="/media/%s" />' % self.image
         else:
             return u'(No image)'
 
