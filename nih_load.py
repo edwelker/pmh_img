d4e64409cd943f531f408133c33cfbd266f8347d
Eddie Welker <eddie.welker@gmail.com>
2013-01-10 13:17:52 -0500

    now added the changes to the NIH images loader

--- nih_load.py
+++ nih_load.py
@@ -15,5 +15,41 @@ parser = etree.XMLParser(ns_clean=True, recover=True)
 files = glob('import/N*.xml')
 for file in files:
     xml = etree.parse(file, parser)
+    
+    center = xml.getroot().tag
 
-    print xml.getroot()
+    for el in xml.getroot():
+        image = name = related_terms = source_url = pmh_figure_source = None
+        blob = alt_text = caption =  orig_figure_source = ''
+
+        if el.tag == 'row':
+            for child in el:
+                if child.tag == 'Filename':
+                    image = 'originals/' + center + '/' + child.text.encode('utf8') + '.jpg'
+                elif child.tag == 'URL_for_original':
+                    source_url = child.text.encode('utf8')
+                elif child.tag == 'Title':
+                    name = child.text.encode('utf8')
+                elif child.tag == 'Description':
+                    caption = child.text.encode('utf8')
+                elif child.tag == 'Alternate_Text':
+                    if child.text:
+                        alt_text = child.text.encode('utf8')
+                    else:
+                        alt_text = ''
+                elif child.tag == 'Display_credit_line':
+                    pmh_figure_source = child.text.encode('utf8')
+                elif child.tag == 'Credit_Line':
+                    orig_figure_source = child.text.encode('utf8')
+                elif child.text:
+                    blob += etree.tostring(child)
+
+#        print "%s      %s      %s     %s      %s      %s       %s\n\n%s\n\n\n\n" % \
+#                    (image, name, source_url, caption, alt_text, pmh_figure_source, orig_figure_source, blob)
+
+        try:
+            image_model = Image.objects.create(image=image,name=name,caption=caption,source_url=source_url, \
+                alt_text=alt_text,pmh_figure_source=pmh_figure_source,orig_figure_source=orig_figure_source, \
+                blob=blob)
+        except Exception, err:
+            print "Image data error: %s, \n%s" % (err, alt_text)
